f3f2641542683909596004e19deeb2c3ed5e341b
axd8282 <axd8282@rit.edu>
2013-11-24 23:58:49 -0500

    Parsing the mdls

--- TraceLink.py
+++ TraceLink.py
@@ -2,15 +2,18 @@
 
 '''Getting the module'''
 import os
+import sys
+import subprocess
 from os import listdir
 from os.path import isfile, join
 
+
 check = 0
+fileLst = []
 
 while(check != 1):
     directory = input("Enter the full (absolute) path of the directory: ")
     if os.path.isdir(directory):    #check if directory exists
-        fileLst = []
         recur = input("Would you like to recursively look through the path? (y) (n): ")
         if recur == "y":
             check = 1
@@ -23,13 +26,23 @@ while(check != 1):
                 tmpPath = join(directory,fileName)  #add the filename to the full path
                 if isfile(tmpPath):
                     fileLst.append(tmpPath)
-                    if len(fileLst) == 0:
-                        print ("Current path have no files")
-                    else:   
-                        for validFile in fileLst:
-                            os.system("file " + validFile)
-                        # Using the above for testing on Linux, we would be using mdls normally
         else:
             print("Not a valid option")
     else:
         print("Not a valid path")
+
+if len(fileLst) == 0:   #Empty list terminate the program
+	sys.exit("Current path have no files")
+	
+for file in fileLst:
+    '''
+    Going through arrary do a mdls check 
+    Prase the mdls to get only the webiste where it was download
+    Create a two dictionary: 
+    1. filename ---> link
+    2. filename ---> no link 
+    '''
+    proc = subprocess.Popen(["mdls", file], stdout=subprocess.PIPE)
+    out = str(proc.communicate()[0])
+    nWord = out.lstrip("b\'")
+    #Prase string to remove \n and look for web link
