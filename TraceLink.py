a15bd3c1076ceae151e9aa61e57e65f60340e5e4
axd8282 <axd8282@rit.edu>
2013-11-27 16:07:45 -0500

    Major update
    1. Fix the recursion 
    2. If current path of has no files, the program terminate
    3. Add in mdls 
    4. Dictionary: value is the file name and keys are the url

--- TraceLink.py
+++ TraceLink.py
@@ -3,6 +3,7 @@
 '''Getting the module'''
 import os
 import sys
+import re
 import subprocess
 from os import listdir
 from os.path import isfile, join
@@ -10,6 +11,7 @@ from os.path import isfile, join
 
 check = 0
 fileLst = []
+dictLink = {}
 
 while(check != 1):
     directory = input("Enter the full (absolute) path of the directory: ")
@@ -19,7 +21,8 @@ while(check != 1):
             check = 1
             for dirName, subdirlist, fileName in os.walk(directory): #Walking through the directory and store full path
                 for name in fileName:
-                    fileLst = dirName + "/" + name
+                    lst= dirName + "/" + name
+                    fileLst.append(lst)
         elif recur == "n":
             check = 1
             for fileName in listdir(directory): #Find all the file at current directory
@@ -32,20 +35,19 @@ while(check != 1):
         print("Not a valid path")
 
 if len(fileLst) == 0:   #Empty list terminate the program
-	sys.exit("Current path have no files")
-	
-for file in fileLst:
-    '''
-    Going through arrary do a mdls check 
-    Prase the mdls to get only the webiste where it was download
-    Create a two dictionary: 
-    1. filename ---> link
-    2. filename ---> no link 
-    '''
-    proc = subprocess.Popen(["mdls", file], stdout=subprocess.PIPE)
-    out = str(proc.communicate()[0])
-    nWord = out.lstrip("b\'")
-    #Parse string to remove \n and look for web link
-    
+        sys.exit("Current path have no files")
+
+for fname in fileLst:		
+    #Use mdls and grep for the url
+    proc1 = subprocess.Popen(["mdls", fname], stdout =subprocess.PIPE)
+    proc2 = subprocess.Popen(["grep", "http"], stdin=proc1.stdout, stdout=subprocess.PIPE)
+    url = str(proc2.communicate()[0])
+
+    if url == "b\'\'": #if there is no url
+	    dictLink[fname] = "Can't determine the source of the website"
+    else:
+	    match = re.findall('(https?://[^\s"]+)', url) #regex for http/https link
+	    dictLink[fname] = match
+
 # Extract distinct (unique) download history locations via the terminal
 downloadHistory = os.system("sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select distinct LSQuarantineDataURLString from LSQuarantineEvent'")
