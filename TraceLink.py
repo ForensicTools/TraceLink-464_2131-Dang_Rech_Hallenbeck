32adeded8ff8a3ba776b6189a8c5c31630755ad1
Marshall Hallenbeck <Marshall.Hallenbeck@gmail.com>
2013-12-03 17:16:34 -0500

    Updated README and TraceLink.py with major changes
    README: Updated bugs, updates, and contact information
    
    TraceLink.py:
    
    * Finished implementation of grabbing download history.
    * Fixed and finished grabbing of mdls file download location
    * Implemented comparison between system download history and mdls
    download history which includes comparison and output
    * Removed unnecessary code such as old download history grabbing and
    regex

--- TraceLink.py
+++ TraceLink.py
@@ -1,4 +1,5 @@
-#!usr/bin/python3.3
+#!usr/local/bin/python3.3
+'''Loading python3 also loads 3.3 if it is available, so just load 3.3 (or 3.3.3)'''
 
 '''Getting the module'''
 import os
@@ -8,14 +9,13 @@ import subprocess
 from os import listdir
 from os.path import isfile, join
 
-
 check = 0
 fileLst = []
 dictLink = {}
 
 while(check != 1):
-    directory = input("Enter the full (absolute) path of the directory: ")
-    if os.path.isdir(directory):    #check if directory exists
+    directory = input("Enter the full path of the directory: ")
+    if os.path.exists(directory) == True:    #check if directory exists
         recur = input("Would you like to recursively look through the path? (y) (n): ")
         if recur == "y":
             check = 1
@@ -37,17 +37,20 @@ while(check != 1):
 if len(fileLst) == 0:   #Empty list terminate the program
         sys.exit("Current path have no files")
 
-for fname in fileLst:		
+# Extract distinct (unique) download history locations via the terminal
+downloadHistory = os.popen("sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select distinct LSQuarantineDataURLString from LSQuarantineEvent'").read()
+
+for fname in fileLst:       
     #Use mdls and grep for the url
     proc1 = subprocess.Popen(["mdls", fname], stdout =subprocess.PIPE)
     proc2 = subprocess.Popen(["grep", "http"], stdin=proc1.stdout, stdout=subprocess.PIPE)
     url = str(proc2.communicate()[0])
 
     if url == "b\'\'": #if there is no url
-	    dictLink[fname] = "Can't determine the source of the website"
+        dictLink[fname] = "Can't determine the source of the website"
     else:
-	    match = re.findall('(https?://[^\s"]+)', url) #regex for http/https link
-	    dictLink[fname] = match
-
-# Extract distinct (unique) download history locations via the terminal
-downloadHistory = os.system("sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'select distinct LSQuarantineDataURLString from LSQuarantineEvent'")
+        link = url.split("\"")[1]
+        histList = downloadHistory.split("\n")
+        for dlink in histList:
+                if link == dlink:
+                        print("The file " + fname + " was found on this system with the download link " + link + " and was downloaded on this system.\n")
